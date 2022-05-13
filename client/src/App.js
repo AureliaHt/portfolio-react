da0d96ea59317976bb3ddf2cb75876f3664ffb39
Aurélia Hemeret <aurelia.hemeret@gmail.com>
2022-05-13 10:23:16 +0200

    adding route

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,17 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Landing from "./components/Landing";
 import AuthenticationForm from "./components/admin/AuthenticationForm";
-
 
 function App() {
   return (
-    <div>
-      <h1>Portfolio react</h1>
-      <AuthenticationForm/>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<Landing/>}/>
+        <Route path="/" element={<Landing/>}/>
+        <Route path="/admin" element={<AuthenticationForm/>}/>
+        <Route path="*" element={<Landing/>}/>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
