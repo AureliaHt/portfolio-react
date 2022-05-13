dbef0828edc7808e4a76d4f01bdf87f4915782a8
Aurélia Hemeret <aurelia.hemeret@gmail.com>
2022-05-13 11:49:50 +0200

    sass design settings

--- client/src/components/admin/AuthenticationForm.js
+++ client/src/components/admin/AuthenticationForm.js
@@ -1,10 +1,31 @@
+import axios from "axios";
+
 const AuthenticationForm = () => {
+
+    const handleLogin = (e) => {
+        e.preventDefault();
+    };
+
     return (
-        <div>
-            <input type="text" placeholder="identifiant" required/>
-            <input type="password" placeholder="mot de passe" required/>
-            <input type="submit" placeholder="se connecter"/>
-        </div>
+        <form onSubmit={handleLogin}>
+            <input 
+                type="text" 
+                id="identifiant"
+                autoComplete="off"
+                placeholder="identifiant" 
+                required
+            />
+            <input 
+                type="password" 
+                id="password"
+                placeholder="mot de passe" 
+                required
+            />
+            <input 
+                type="submit" 
+                value="se connecter"
+            />
+        </form>
     );
 };
 
